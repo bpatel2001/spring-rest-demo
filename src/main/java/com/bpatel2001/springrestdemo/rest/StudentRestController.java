f7c38103f3886116b41d4f3c143ae446a3d893f8
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-06-16 11:31:54 -0500

    Created a get by student ID endpoint as well as the exception handling for it

--- src/main/java/com/bpatel2001/springrestdemo/rest/StudentRestController.java
+++ src/main/java/com/bpatel2001/springrestdemo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.bpatel2001.springrestdemo.rest;
 
 import com.bpatel2001.springrestdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +13,57 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // Define endpoint for /students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    // Define @PostConstruct to load the student data
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
 
         // This will convert from POJO to JSON
         theStudents.add(new Student("Bhavik", "Patel"));
         theStudents.add(new Student("John", "Doe"));
         theStudents.add(new Student("Hank", "Hill"));
 
+    }
+
+    // Define endpoint for /students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         return theStudents;
+    }
+
+    // Define endpoint for /students/{studentId}
+
+    @GetMapping("/students/{studentId}")
+    // By default, the variables should match between the name of the mapping you are trying to do and the object passed in: {studentId} -> int studentId
+    public Student getStudent(@PathVariable int studentId) {
+        // Index from the list
+
+        // Check the studentID against list size - throw StudentNotFoundException if not found
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException(("Student id not found - " + studentId));
+        }
+        return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        // Create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // Return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
